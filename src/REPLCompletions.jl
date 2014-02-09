1d85f4e567b418c1aadf66d38226ae28c84711d2
Keno Fischer <kfischer@college.harvard.edu>
2014-02-09 17:31:28 -0500

    Complete package names after `using`

--- src/REPLCompletions.jl
+++ src/REPLCompletions.jl
@@ -9,7 +9,7 @@ module REPLCompletions
     end
 
     # REPL Symbol Completions
-    function complete_symbol(sym)
+    function complete_symbol(sym,ffunc)
         # Find module
         strs = split(sym,".")
         # Maybe be smarter in the future
@@ -63,19 +63,26 @@ module REPLCompletions
 
         suggestions = String[]
         if lookup_module
+            # We will exlcude the results that the user does not want, as well
+            # as excluding Main.Main.Main, etc., because that's most likely not what
+            # the user wants
+            p = s->(ffunc(mod,s) && s != module_name(mod))
             # Looking for a binding in a module
             if mod == context_module
                 # Also look in modules we got through `using` 
                 mods = ccall(:jl_module_usings,Any,(Any,),Main)
                 for m in mods
                     ssyms = names(m)
+                    filter!(p,ssyms)
                     syms = map!(string,Array(UTF8String,length(ssyms)),ssyms)
                     append!(suggestions,syms[map((x)->completes_global(x,name),syms)])
                 end
                 ssyms = names(mod,true,true)
+                filter!(p,ssyms)
                 syms = map!(string,Array(UTF8String,length(ssyms)),ssyms)
             else 
                 ssyms = names(mod,true,false)
+                filter!(p,ssyms)
                 syms = map!(string,Array(UTF8String,length(ssyms)),ssyms)
             end
             append!(suggestions,syms[map((x)->completes_global(x,name),syms)])
@@ -84,12 +91,12 @@ module REPLCompletions
             fields = t.names
             for field in fields
                 s = string(field)
-                if beginswith(s,name)
+                if beginswith(s,name) && 
                     push!(suggestions,s)
                 end
             end
         end
-        sort(unique(suggestions))
+        suggestions
     end
 
     const non_word_chars = " \t\n\"\\'`@\$><=:;|&{}()[].,+-*/?%^~"
@@ -112,13 +119,33 @@ module REPLCompletions
             end
             startpos = prevind(string,startpos)
         end
+        ffunc = (mod,x)->true
+        suggestions = UTF8String[]
+        r = rsearch(string,"using",startpos)
+        if all(isspace,string[nextind(string,last(r)):prevind(string,startpos)])
+            # We're right after using. Let's look only for packages
+            # and modules we can reach form here
+
+            # If there's no dot, we're in toplevel, so we should
+            # also search for packages
+            s = string[startpos:pos]
+            if dotpos == 0
+                append!(suggestions,filter(pname->begin
+                    pname[1] != '.' &&
+                    pname != "METADATA" &&
+                    beginswith(pname,s)
+                end,readdir(Pkg.dir())))
+            end
+            ffunc = (mod,x)->(isdefined(mod,x) && isa(mod.(x),Module))
+        end
         if startpos == 0
             pos = -1
         end
         if dotpos == 0
             dotpos = startpos-1
         end
-        complete_symbol(string[startpos:pos]), (dotpos+1):pos
+        append!(suggestions,complete_symbol(string[startpos:pos],ffunc))
+        sort(unique(suggestions)), (dotpos+1):pos
     end
 
     function shell_completions(string,pos)
